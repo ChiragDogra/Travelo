2b9f41d948aaec63940e5eb6ed3dde59cde1f62a
Chirag Dogra <chirag.dogra79@gmail.com>
2022-08-08 15:37:56 +0530

    build folder build

---
+++ build/precache-manifest.64f3db01a6aaa64a53b8228b5eb5ab7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b943158c13a4533edf684c09fce79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "488e56e20b3a1ec83121",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "8967c0b471e5a9cc23c6",
+    "url": "/static/js/2.96b563bd.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.96b563bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488e56e20b3a1ec83121",
+    "url": "/static/js/main.47ec3f8e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.47ec3f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);

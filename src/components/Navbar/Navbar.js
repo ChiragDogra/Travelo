27202fa312264fa6cac9df1a7c5ac189ebc55c99
Chirag Dogra <chirag.dogra79@gmail.com>
2022-08-09 15:46:37 +0530

    some visual changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,7 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
-import memoriesLogo from '../../images/memoriesLogo.png';
-import memoriesText from '../../images/memoriesText.png';
+import travelloLogo from '../../images/TravelloLogo.png';
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
 
@@ -39,8 +38,7 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <Link to="/" className={classes.brandContainer}>
-        <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
-        <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+        <img className={classes.image} src={travelloLogo} alt="icon" height="40px" />
       </Link>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
